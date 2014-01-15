8a3c364597150447b5b2fc3fb41bb5c07a7b4a0f
Aaron Stuart Grant <blamonet@gmail.com>
2014-01-14 23:13:13 -0500

    Added swap case C++ solution

---
+++ swap_case.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, const char* argv[])
+{
+  std::string line;
+  std::ifstream myfile (argv[1]);
+  int i = 0;
+  char c;
+
+  if (myfile.is_open()) {
+    while (std::getline(myfile,line)) {
+      i = 0;
+      while(line[i]) {
+        c = line[i];
+        if (isupper(c)) {
+           line[i] = tolower(c);
+        } else if (islower(c)) {
+           line[i] = toupper(c);
+        }
+      i++;
+      }
+      std::cout << line << std::endl;
+    }
+    myfile.close();
+  }
+
+  return 0;
+}
